3750367f5ce8035cef5e1abbf0f7747c31d038b6
W-ptr <wisnu.putra001@binus.ac.id>
2024-08-04 20:58:11 +0700

    upload template

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express()
+const apiController = require('./controller/apiController');
 require("dotenv").config()
+const app = express()
 
 app.use(morgan('combined'));
 app.use(express.json())
 
+app.use('/api',apiController);
+
 app.get('/',(req,res)=>{
     res.send("test 123");
+});
+
+app.use((req,res)=>{
+    res.status(404).send("<h1>404 Not Found</h1>");
 });
 
 app.listen(process.env.PORT,"0.0.0.0",()=>{
---
+++ Backend/controller/apiController.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.post('/auth/register',async (req,res)=>{
+    
+});
+
+module.exports = router;

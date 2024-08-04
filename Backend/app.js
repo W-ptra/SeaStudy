ef6038b38e22d22af7b0279196ffbc8e0091fcdd
W-ptr <wisnu.putra001@binus.ac.id>
2024-08-04 20:22:09 +0700

    upload template

--- Backend/app.js
+++ Backend/app.js
@@ -1,2 +1,15 @@
 const express = require('express');
-console.log("test")
+const morgan = require('morgan');
+const app = express()
+require("dotenv").config()
+
+app.use(morgan('combined'));
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send("test 123");
+});
+
+app.listen(process.env.PORT,"0.0.0.0",()=>{
+    console.log(`listening to port ${process.env.PORT}`);
+})

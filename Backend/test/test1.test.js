d1bf2e35b31e290208b97a0d5e12911d7413f26f
W-ptr <wisnu.putra001@binus.ac.id>
2024-08-07 16:04:12 +0700

    feat: add unitest using jest for testing hashing and compare function

---
+++ Backend/test/test1.test.js
@@ -0,0 +1,11 @@
+const {comparePassword,hashPassword} = require("../helper/hashing");
+
+test("hash passworld 123 and compare with 123",async ()=>{
+    const password = "123"
+    const hash = await hashPassword(password);
+    const isMatch = await comparePassword(password,hash);
+
+    expect(isMatch).toBe(true)
+})
+
+//tets("Login with 'test@yahoo' & password '123' ")

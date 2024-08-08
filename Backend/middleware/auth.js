add018945f75d80a1bd6b1552aebf6757a0406f5
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-08-08 19:47:37 +0700

    refactor(auth): modularize jwt operations

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,19 +1,25 @@
 const jwt = require('jsonwebtoken');
+const { validateJSONToken } = require('../helper/jwt');
 require("dotenv").config();
 
-function authenticateJWT(req,res,next){
+const authenticateJWT = (allowedRole) => (req,res,next) => {
     const token = req.cookies.token;
 
     if (!token)
         return res.status(401).json({message:"Token is required"});
 
-    jwt.verify(token,process.env.KEY,(err,user)=>{
-        if(err){
-            res.cookie('token','',{httpOnly:true,secure:true,expires: new Date(0)});
-            return res.status(403).json({message:"Forbidden"});
+    try {
+        const decoded = validateJSONToken(token);
+        
+        if(decoded.role !== allowedRole){
+            return res.status(403).json({ message: "Forbidden access" });
         }
+
+        req.user = decoded;
         next();
-    });
+    } catch (err) {
+        return res.status(401).json({message:"Unauthorized"});
+    }
 }
 
 module.exports = {authenticateJWT};

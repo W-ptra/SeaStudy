04a3bbacdefa224fcb4689f6d34af4f4f3f122ff
AtqiyaHaydar <ashford1524@gmail.com>
2024-08-12 19:54:21 +0700

    feat: initial page of course and dashboard

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
-import { LogIn } from 'lucide-react';
-import { Button } from './ui/button';
-import { GraduationCap } from 'lucide-react';
-
-import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
+
+// Component Import
+import { Button } from './ui/button';
+
+// Icon Import
+import { LogIn } from 'lucide-react';
+import { GraduationCap } from 'lucide-react';
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -32,17 +35,30 @@ const Navbar = () => {
      'fixed top-0 left-0 w-full bg-white z-20', isScrolled ? "border-b-2 border-black" : "border-none"
     )}>
       <header className='wrapper flex items-center justify-between'>
+        {/* App Logo */}
         <h1 className='text-xl font-bold flex items-center gap-x-4'>
           <GraduationCap className='w-8 h-8' /> SeaStudy
         </h1>
-        <nav>
-          {/* IF THE USER HASN'T LOGGED IN */}
-          <Link href="/sign-in">
-            <Button className='w-[120px] hidden md:flex items-center justify-center rounded-full gap-2 hover:gap-4 transition-all'>
-              Sign In <LogIn className='w-5 h-5' />
-            </Button>
+
+        {/* Navigation */}
+        <nav className='flex items-center justify-center gap-x-4'>
+          <Link href="/">
+            Home
+          </Link>
+          <Link href="/dashboard">
+            Dashboard
+          </Link>
+          <Link href="/courses">
+            Courses
           </Link>
         </nav>
+
+        {/* Sign In Button (IF THE USER HASN'T LOGGED IN) */}
+        <Link href="/sign-in">
+          <Button className='w-[120px] hidden md:flex items-center justify-center rounded-full gap-2 hover:gap-4 transition-all'>
+            Sign In <LogIn className='w-5 h-5' />
+          </Button>
+        </Link>
       </header>
     </div>
   )
